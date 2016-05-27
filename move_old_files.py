ae82cb9efff1baca420d514daf5332cff30362ff
Dmitry Tolkach <chakmidlot@gmail.com>
2016-05-28 01:00:05 +0300

    Added special extensions

--- move_old_files.py
+++ move_old_files.py
@@ -6,11 +6,14 @@ import os
 import shutil
 import sys
 
+__version__ = "0.1.0"
+
 
 parser = argparse.ArgumentParser(description='Moves old files from one directory to another.')
 parser.add_argument('-s', '--src', required=True, help='source directory')
 parser.add_argument('-d', '--dst', required=True, help='destination directory')
 parser.add_argument('-t', '--threshold-days', type=int, required=True,  help='days since last access to file')
+parser.add_argument('-V', '--version', action='version', version='Version: {}'.format(__version__))
 
 args = parser.parse_args()
 
@@ -22,6 +25,12 @@ lifetime = args.threshold_days * 24 * 3600
 
 
 deadline = datetime.utcnow().timestamp() - lifetime
+
+special_extensions = [
+    '.tar.gz',
+    '.tar.xz',
+    '.tar.bz2'
+]
 
 
 def move():
@@ -47,6 +56,24 @@ def prepare_name(path):
     dir_name = os.path.dirname(path)
     base_name = os.path.basename(path)
 
+    name, ext = split_name(base_name)
+
+    i = 1
+    while True:
+        new_path = os.path.join(dir_name, '{}({}){}'.format(name, i, ext))
+        if not os.path.exists(new_path):
+            return new_path
+        i += 1
+
+
+def split_name(base_name: str):
+
+    for spec_ext in special_extensions:
+        if base_name.endswith(spec_ext):
+            name = base_name[:-len(spec_ext)]
+            ext = base_name[-len(spec_ext):]
+            return name, ext
+
     if '.' not in base_name:
         name = base_name
         ext = ''
@@ -54,13 +81,7 @@ def prepare_name(path):
         name = '.'.join(base_name.split('.')[:-1])
         ext = '.' + base_name.split('.')[-1]
 
-    print(name)
-    i = 1
-    while True:
-        new_path = os.path.join(dir_name, '{}({}){}'.format(name, i, ext))
-        if not os.path.exists(new_path):
-            return new_path
-        i += 1
+    return name, ext
 
 
 if __name__ == '__main__':
